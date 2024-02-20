ea131feec1b7c5869531712c8ef0ef93c30fcb78
vhenriquebm <vhenriquebm@gmail.com>
2024-02-20 17:41:41 -0300

    Ids configurados, viewBinding adicionado

--- app/src/main/java/com/example/gastoviagemandroid/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagemandroid/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.gastoviagemandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.example.gastoviagemandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configureButton()
+    }
+
+    private fun configureButton() {
+        binding.buttonCalculate.setOnClickListener {
+            println("The button is working")
+        }
     }
 }
